e199125ea675f42d72d3f7e8434f82f385325359
Wendy Harris <wendyv510@hotmail.com>
2019-09-22 21:05:07 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -11,6 +11,9 @@ def monopoly_with_third_tier
   monopoly_with_third_tier = [railroads][pieces]=rent_in_dollars 
   {:rent_in_dollars =>
   {:one_piece_owned =>25, :two_pieces_owned =>50, :three_pieces_owned =>100, :four_pieces_owned => 200}}
+  {:railroads => 
+  {:names => 
+  {:reading_railroad => {}, :pennsylvania_railroad => {}, :b_and_o_railroad => {}, :shortline_railroad => {}}}
 end
 
 def monopoly_with_fourth_tier
